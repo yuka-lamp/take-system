67ec28e1411a880da485c3346bab3a2df7e01b12
yuka-lamp <oonishi@amp>
2021-02-01 17:08:52 +0900

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -43,24 +43,24 @@ $(function ($) {
   });
 
   // スムーススクロール
-  $(".smooth").on("click", function () {
-    var speed = 500;
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? "html" : href);
-    var position = target.offset().top - 100;
-    $("html, body").animate({ scrollTop: position }, speed, "swing");
-    return false;
-  });
+  // $(".smooth").on("click", function () {
+  //   var speed = 500;
+  //   var href = $(this).attr("href");
+  //   var target = $(href == "#" || href == "" ? "html" : href);
+  //   var position = target.offset().top - 100;
+  //   $("html, body").animate({ scrollTop: position }, speed, "swing");
+  //   return false;
+  // });
 
   // スクロールイベント
-  $(window).on("scroll", function () {
-    let s = $(window).scrollTop();
-    if (s > 300) {
-      $(".header .navbar").addClass("scroll");
-    } else {
-      $(".header .navbar").removeClass("scroll");
-    }
-  });
+  // $(window).on("scroll", function () {
+  //   let s = $(window).scrollTop();
+  //   if (s > 300) {
+  //     $(".header .navbar").addClass("scroll");
+  //   } else {
+  //     $(".header .navbar").removeClass("scroll");
+  //   }
+  // });
 
   // seo
   $(".ac dt").on("click", function() {

c888ef5af9bcf63dcef6fda14562ded654ce853b
yuka-lamp <oonishi@amp>
2021-02-09 13:36:56 +0900

    update

--- header.php
+++ header.php
@@ -16,11 +16,11 @@ $img_url = get_template_directory_uri().'/dist/images/';
   <?php wp_head(); ?>
   <?php if (!is_user_logged_in()): ?>
   <!-- Google Tag Manager -->
-  <!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-WSTPTV7');</script> -->
+  })(window,document,'script','dataLayer','GTM-WSTPTV7');</script>
   <!-- End Google Tag Manager -->
   <?php endif; ?>
 </head>
